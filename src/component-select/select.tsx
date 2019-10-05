7697927975196eff55ae27dbdd1e1727a6c36ea8
diberry <diberry@microsoft.com>
2019-10-04 19:20:04 -0700

    first commit

---
+++ src/component-select/select.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class DxbSelect extends React.Component {
+    render() {
+      return (
+        <div className="dxb-select">
+          select component c
+        </div>
+      );
+    }
+  }
